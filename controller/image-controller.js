fc41c51f86d6c9d5bf0ec01c9ca55c9761a7ea55
rayhansheikh99 <rsheikh201031@msce.uiu.ac.bd>
2022-02-27 22:25:29 +0600

    product updated

---
+++ controller/image-controller.js
@@ -0,0 +1,14 @@
+import grid from 'gridfs-stream';
+import mongoose from 'mongoose';
+
+const url = 'http://localhost:5000';
+
+
+export const uploadImage = (request, response) => {
+    if(!request.file) 
+        return response.status(404).json("File not found");
+    
+    const imageUrl = `${url}/file/${request.file.filename}`;
+
+    response.status(200).json(imageUrl);    
+}
